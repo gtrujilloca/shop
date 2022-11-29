017abf3b872698adb27e6b4008b1aa6d421a469e
Carlos García Trujillo <70419615+gtrujilloca@users.noreply.github.com>
2022-11-29 18:30:52 -0500

    UPDATE:
    Add white space

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const routes = require('./routes/index.routes')
 
 app.use('/api/', routes);
 
+
 app.listen(app.get('PORT'), () => {
   console.log(`Server is listening http://localhost:${app.get('PORT')}/api`);
 })
